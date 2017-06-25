5df7cb1e9de4b6bd621682b56098d2609a91e49a
stelio92 <st.tzavelas@gmail.com>
2017-06-25 20:30:42 +0200

    DatePicker
    DatePicker in der Allgemein Activity implementiert

--- Travlhoe/app/src/main/java/com/group6/travlhoe/activity_Allgemein.java
+++ Travlhoe/app/src/main/java/com/group6/travlhoe/activity_Allgemein.java
@@ -1,5 +1,6 @@
 package com.group6.travlhoe;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
+import android.text.InputType;
 import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.ColorRes;
@@ -14,16 +16,32 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 /**
  * Created by STzavelas on 24.06.17.
  */
 
-public class activity_Allgemein extends AppCompatActivity {
+public class activity_Allgemein extends AppCompatActivity implements View.OnClickListener{
 
 
     private BottomNavigationView bottomNavigationView;
+
+    private EditText DatumVon;
+    private EditText DatumBis;
+
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +49,6 @@ public class activity_Allgemein extends AppCompatActivity {
         setContentView(R.layout.activity_allgemein);
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-
-
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
             @Override
@@ -52,7 +68,59 @@ public class activity_Allgemein extends AppCompatActivity {
 
         bottomNavigationView.setSelectedItemId(R.id.menu_allgemein);
 
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.GERMAN);
+
+        findViewsById();
+
+        setDateTimeField();
+
+    }
+
+    private void findViewsById() {
+        DatumVon = (EditText) findViewById(R.id.eTxt_DatumVon);
+        DatumVon.setInputType(InputType.TYPE_NULL);
+        DatumVon.requestFocus();
+
+        DatumBis = (EditText) findViewById(R.id.eTxt_DatumBis);
+        DatumBis.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField(){
+        DatumVon.setOnClickListener(this);
+        DatumBis.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                DatumVon.setText(dateFormatter.format(newDate.getTime()));
+
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                DatumBis.setText(dateFormatter.format(newDate.getTime()));
+
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        if(view == DatumVon) {
+            fromDatePickerDialog.show();
+        } else if(view == DatumBis) {
+            toDatePickerDialog.show();
+        }
+
+    }
 }
