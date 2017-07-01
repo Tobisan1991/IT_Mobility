11dcac833a2f8b7e9b3d2e10def2b5241f628733
Daniel <d.koeber@online.de>
2017-07-01 14:15:17 +0200

    beste

--- Travlhoe/app/src/main/java/com/group6/travlhoe/MainActivity.java
+++ Travlhoe/app/src/main/java/com/group6/travlhoe/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_old);
 
         btnTransport = (Button) findViewById(R.id.button1);
         btnAllgemein = (Button) findViewById(R.id.button2);
