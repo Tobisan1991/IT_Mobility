86ec1c9a47bc0151571db73cadaf21d1ee5fc35c
ripane <ripane@hotmail.de>
2017-08-18 19:19:17 +0200

    Such Funktion eingebaut Google Maps
    meysichh

--- Travlhoe/app/src/main/java/com/group6/TakeOff/MapsActivity.java
+++ Travlhoe/app/src/main/java/com/group6/TakeOff/MapsActivity.java
@@ -2,20 +2,43 @@ package com.group6.TakeOff;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnMarkerDragListener
+        {
 
     private GoogleMap mMap;
+    double end_latidude, end_longitude;
+    double latitude,longitude;
+    private Location lastlocation;
+    private GoogleApiClient client;
+    private Marker currentLocationmMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +49,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+/*
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        lastlocation = location;
+        if(currentLocationmMarker != null)
+        {
+            currentLocationmMarker.remove();
+
+        }
+        Log.d("lat = ",""+latitude);
+        LatLng latLng = new LatLng(location.getLatitude() , location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        currentLocationmMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+
+        if(client != null)
+        {
+            LocationServices.FusedLocationApi.removeLocationUpdates(client,this);
+        }
+
+    }
+    */
+
+    public void onClick(View v){
+        if(v.getId()==R.id.Bsearch){
+
+            EditText tf = (EditText) findViewById(R.id.TFaddress);
+            String location = tf.getText().toString();
+            List<Address> addressList;
+
+
+            if(!location.equals("")) {
+                Geocoder geocoder = new Geocoder(this);
+
+                try {
+                    addressList = geocoder.getFromLocationName(location, 5);
+
+                    if(addressList != null)
+                    {
+                        for(int i = 0;i<addressList.size();i++)
+                        {
+                            LatLng latLng = new LatLng(addressList.get(i).getLatitude() , addressList.get(i).getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            markerOptions.position(latLng);
+                            markerOptions.title(location);
+                            mMap.addMarker(markerOptions);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        /*
+        if(v.getId()==R.id.bTo){
+            mMap.clear();
+            MarkerOptions markerOptions= new MarkerOptions();
+            markerOptions.position(new LatLng(end_latidude, end_longitude));
+            markerOptions.title("Destination");
+            markerOptions.draggable(true);
+
+            float result [] = new float[10];
+
+            Location.distanceBetween(latitude, longitude, end_latidude, end_longitude, result);
+            markerOptions.snippet("distance "+ result[0]);
+            mMap.addMarker(markerOptions);
+
+
+
+
+        }*/
+    }
+
+
 
 
     /**
@@ -57,7 +162,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+/*
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            bulidGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
 
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+        */
 
     }
+
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.setDraggable(true);
+        return false;
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+        end_latidude= marker.getPosition().latitude;
+        end_longitude=marker.getPosition().longitude;
+    }
+
+
 }
